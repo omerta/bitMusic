1966b2125231725ca2d656bdc9ae50a46056a456
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-03-06 17:33:19 -0500

    Creación de componente sign-up

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor(
+    private UserService: UserService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  signUp() {
+  }
+
+}
