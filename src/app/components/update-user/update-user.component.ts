b12c47bd48c46a792eedd6f3d050ff9fd40b734c
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-03-07 09:14:13 -0500

    Creado el componente para modificar el usuario.

---
+++ src/app/components/update-user/update-user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-update-user',
+  templateUrl: './update-user.component.html',
+  styleUrls: ['./update-user.component.css']
+})
+export class UpdateUserComponent implements OnInit {
+  public user: User;
+  constructor(
+    private service: UserService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
